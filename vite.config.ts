2c85db13e00cde4011a9dc39ddda070b240609a2
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2024-05-16 13:11:09 +1100

    Legacy плагин

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: 'defaults, android >= 5.0, ios >= 8',
+    })
+  ],
 
 
   root: './src',
