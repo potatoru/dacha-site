71865362c2a2e6a74f4e1ed91130f6b55ddfe682
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2024-05-17 15:48:00 +1100

    Нескучные картинки

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,44 +1,50 @@
 <template>
   <div class="container h-100 d-flex align-items-center justify-content-center">
     <div class="col-md-6 col-12 mx-auto">
-      <div class="p-4 rounded bg-light shadow-sm">
-        <p class="d-flex align-items-center">
-          <span style="width: 2.25rem" class="fs-5">&#127969;</span>Добро пожаловать на Дачу
-        </p>
-        <div style="margin-left: 2.25rem">
-          <p>Дача — это ванильный сервер Minecraft для общения и игры с друзьями без лишних плагинов и дополнительных режимов.</p>
-          <p>Из возможностей на сервере есть только телепорт домой и приват территории от гриферов.</p>
+      <div class="rounded-2 bg-light shadow-sm overflow-hidden">
+        <div class="sky" />
+
+        <div class="p-4">
+          <p class="d-flex align-items-center">
+            <span style="width: 2.25rem" class="fs-5">&#127969;</span>Добро пожаловать на Дачу
+          </p>
+          <div style="margin-left: 2.25rem">
+            <p>Дача — это ванильный сервер Minecraft для общения и игры с друзьями без лишних плагинов и дополнительных режимов.</p>
+            <p>Из возможностей на сервере есть только телепорт домой и приват территории от гриферов.</p>
+          </div>
+          <p class="d-flex align-items-center">
+            <span style="width: 2.25rem" class="fs-5">&#128268;</span>Как подключиться
+          </p>
+          <div class="mb-2" style="margin-left: 2.25rem">
+            <table class="table table-borderless table-sm mb-0">
+              <tbody>
+                <tr>
+                  <td class="bg-light pt-0 ps-0">IP</td>
+                  <td class="bg-light pt-0 ps-0">
+                    <div class="d-flex align-items-center">
+                      <span class="text-decoration-underline">playdacha.ru</span>
+                      <span class="ms-2 status" :class="{online: status == 1}" v-if="status > -1" />
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="bg-light ps-0">Версия</td>
+                  <td class="bg-light ps-0">1.20.x</td>
+                </tr>
+                <tr>
+                  <td class="bg-light ps-0">Discord</td>
+                  <td class="bg-light ps-0"><a href="https://playdacha.ru/discord" target="_blank">playdacha.ru/discord</a></td>
+                </tr>
+                <tr>
+                  <td class="bg-light ps-0 pb-0">ВК</td>
+                  <td class="bg-light ps-0 pb-0"><a href="https://vk.com/playdacha" target="_blank">vk.com/playdacha</a></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-        <p class="d-flex align-items-center">
-          <span style="width: 2.25rem" class="fs-5">&#128268;</span>Как подключиться
-        </p>
-        <div class="mb-2" style="margin-left: 2.25rem">
-          <table class="table table-borderless table-sm">
-            <tbody>
-              <tr>
-                <td class="bg-light pt-0 ps-0">IP</td>
-                <td class="bg-light pt-0 ps-0">
-                  <div class="d-flex align-items-center">
-                    <span class="text-decoration-underline">playdacha.ru</span>
-                    <span class="ms-2 status" :class="{online: status == 1}" v-if="status > -1" />
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="bg-light ps-0">Версия</td>
-                <td class="bg-light ps-0">1.20.x</td>
-              </tr>
-              <tr>
-                <td class="bg-light ps-0">Discord</td>
-                <td class="bg-light ps-0"><a href="https://playdacha.ru/discord" target="_blank">playdacha.ru/discord</a></td>
-              </tr>
-              <tr>
-                <td class="bg-light ps-0">ВК</td>
-                <td class="bg-light ps-0"><a href="https://vk.com/playdacha" target="_blank">vk.com/playdacha</a></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+
+        <div class="ground" />
       </div>
     </div>
   </div>
