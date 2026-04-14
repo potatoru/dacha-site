81ecf2b6ea19c3b8fc7e6bef783f6fc19b4bed6c
Alexander <9192541+potatoru@users.noreply.github.com>
2026-04-14 18:32:23 +1100

    Smaller padding

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-column min-h-screen ">
-    <header class="text-white px-3 py-2">
+    <header class="text-white px-2 py-2">
       <div class="site-container mx-auto">
         <SiteHeader />
       </div>
     </header>
 
-    <main class="flex-grow-1 p-4 site-container mx-auto w-full">
+    <main class="flex-grow-1 p-3 site-container mx-auto w-full">
       <router-view />
     </main>
 
